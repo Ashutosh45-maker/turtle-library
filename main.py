6a9591c8ce67d3e2f39d305083106891f1a04f64
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-20 20:58:29 +0530

    intro to turtle

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.Screen().bgcolor("blueviolet")
+sc=turtle.Screen()
+sc.setup(500,500)
+turtle.title("welcome to turtle website")
+board=turtle.Turtle()
+board.fillcolor("red")
+board.begin_fill()
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+
+board.end_fill()
+
+turtle.done()
